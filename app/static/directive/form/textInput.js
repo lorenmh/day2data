9816ddc560f1a9d2a6edb1040287faa5cdf4b8d2
Loren <neutronstar@gmail.com>
2015-01-27 22:52:03 -0800

    added io-text-input, io-integer-input, io-number-input, need to add selects, etc

--- app/static/directive/form/textInput.js
+++ app/static/directive/form/textInput.js
@@ -2,16 +2,20 @@ angular.module('app').directive('ioTextInput', function() {
   return {
     restrict: 'E',
     scope: {
-      inputValue: '=ngModel'
+      ioValue: '=',
+      ioError: '='
     },
     replace: true,
     template: 
-      '<div class="io-input-wrap">' +
+      '<div class="io-input-container io-text">' +
         '<label class="io-input-label">' +
           '<span class="io-input-label-text">{{ label }}:' +
-            '<span class="{{ requiredClass() }}">{{ requiredText() }}</span>' +
+            '<span ng-if="required" class="io-input-required">*</span>' +
           '</span>' +
-          '<input class="io-input" type="text" ng-model="inputValue" placeholder="{{ placeholder }}"/>' +
+          '<div class="io-input-wrap">' +
+            '<span ng-if="ioError" class="io-error-text">{{ ioError }}</span>' +
+            '<input class="io-input"  type="text" ng-model="ioValue" ng-change="validate()" placeholder="{{ placeholder }}" />' +
+          '</div>' +
         '</label>' +
       '</div>',
     link: function(scope, el, attr) {
@@ -19,12 +23,82 @@ angular.module('app').directive('ioTextInput', function() {
       scope.placeholder = attr.placeholder || '';
       scope.required = attr.required !== undefined;
 
-      scope.requiredClass = function requiredClass() {
-        return "io-input-" + (scope.required ? 'required' : 'optional')
-      };
+      scope.validate = function() {
 
-      scope.requiredText = function requiredText() {
-        return (scope.required ? '*' : '(optional)');
+      }
+    }
+  };
+});
+
+angular.module('app').directive('ioIntegerInput', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      ioValue: '=',
+      ioError: '='
+    },
+    replace: true,
+    template: 
+      '<div class="io-input-container io-integer">' +
+        '<label class="io-input-label">' +
+          '<span class="io-input-label-text">{{ label }}:' +
+            '<span ng-if="required" class="io-input-required">*</span>' +
+          '</span>' +
+          '<div class="io-input-wrap">' +
+            '<span ng-if="ioError" class="io-error-text">{{ ioError }}</span>' +
+            '<input class="io-input"  type="text" ng-model="ioValue" ng-change="validate()" placeholder="{{ placeholder }}" />' +
+          '</div>' +
+        '</label>' +
+      '</div>',
+    link: function(scope, el, attr) {
+      scope.label = attr.label;
+      scope.placeholder = attr.placeholder || '';
+      scope.required = attr.required !== undefined;
+
+      scope.validate = function() {
+        if (isNaN( +scope.ioValue )) {
+          scope.ioError = 'Please enter number value.'
+        } else if ( (+scope.ioValue) % 1 !== 0) {
+          scope.ioError = 'Please enter a number without decimals'
+        } else {
+          scope.ioError = null;
+        }
+      }
+    }
+  };
+});
+
+angular.module('app').directive('ioNumberInput', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      ioValue: '=',
+      ioError: '='
+    },
+    replace: true,
+    template: 
+      '<div class="io-input-container io-number">' +
+        '<label class="io-input-label">' +
+          '<span class="io-input-label-text">{{ label }}:' +
+            '<span ng-if="required" class="io-input-required">*</span>' +
+          '</span>' +
+          '<div class="io-input-wrap">' +
+            '<span ng-if="ioError" class="io-error-text">{{ ioError }}</span>' +
+            '<input class="io-input" type="text" ng-model="ioValue" ng-change="validate()" placeholder="{{ placeholder }}" />' +
+          '</div>' +
+        '</label>' +
+      '</div>',
+    link: function(scope, el, attr) {
+      scope.label = attr.label;
+      scope.placeholder = attr.placeholder || '';
+      scope.required = attr.required !== undefined;
+
+      scope.validate = function() {
+        if (isNaN( +scope.ioValue )) {
+          scope.ioError = 'Please enter number value.'
+        } else {
+          scope.ioError = null;
+        }
       }
     }
   };
